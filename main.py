8c63d1be63f1de7eea5b4e5981ea7ad2765dd130
Igor Fukin <igor.fukin@yandex.ru>
2023-11-20 11:35:07 +0300

    Внесли изменение в суперфункцию

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 def my_superfunction():
-    print('What an awesome print!')
+    print('Hello Python!')
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
